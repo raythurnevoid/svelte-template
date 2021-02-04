26a4f1b330a8b66aa3f9634469a49fe7a73dd584
Ray Thurne Void <raythurnevoid@gmail.com>
2021-02-04 17:48:43 +0100

    0.0.14: passing compilerOptions as plain object

--- build/module/rules/loaders/tsLoader.ts
+++ build/module/rules/loaders/tsLoader.ts
@@ -1,10 +1,14 @@
 import type { Options } from "ts-loader";
+import { readTsConfig } from "../../../utils/readTsConfig";
 
 export function tsLoader() {
 	return {
 		loader: "ts-loader",
 		options: {
 			allowTsInNodeModules: true,
+			compilerOptions: {
+				...readTsConfig(),
+			},
 		} as Options,
 	};
 }
