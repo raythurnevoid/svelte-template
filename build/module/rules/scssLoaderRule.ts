2b7fa78951f36eb42416fc02159b4ec5a2d81b68
Ray Thurne Void <ray.thurne.void@gmail.com>
2021-04-10 20:28:37 +0200

    0.0.23: Allow remove style-loader when on server

--- build/module/rules/scssLoaderRule.ts
+++ build/module/rules/scssLoaderRule.ts
@@ -2,12 +2,20 @@ import type { BaseInput } from "../../types";
 import { cssLoader, scssLoader } from "./loaders";
 
 export function scssLoaderRule(input: SvelteLoaderRuleInput) {
+	const loaders = [];
+
+	if (!input.server) {
+		loaders.push("style-loader");
+	}
+
 	// TODO: MiniCssExtractPlugin.loader
 	return {
 		test: /\.s?css$/,
 		exclude: /module\.s?css$/,
-		use: ["style-loader", cssLoader(input), scssLoader(input)],
+		use: [...loaders, cssLoader(input), scssLoader(input)],
 	};
 }
 
-interface SvelteLoaderRuleInput extends BaseInput {}
+interface SvelteLoaderRuleInput extends BaseInput {
+	server?: boolean;
+}
